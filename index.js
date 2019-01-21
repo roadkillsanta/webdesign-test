49c34e6c24976d10580101218bfff4ef4e1a2ac7
roadkillsanta <ericnie7@gmail.com>
2019-01-20 22:50:00 -0500

    s

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+Integer parallaxPage = 0;
+
+function setPage(Integer pagenum){
+    console.log(pagenum)
+}
+
+inView('.intro').on('enter', console.log(1));
+inView('.static1').on('enter', console.log(2));
+inView('.bg2').on('enter', console.log(3));
+inView('.static2').on('enter', console.log(4));
