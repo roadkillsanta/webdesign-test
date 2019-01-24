ebc8c4eea05a08eafd640ca75c3a147f36385bb0
roadkillsanta <ericnie7@gmail.com>
2019-01-24 14:15:31 -0500

    s

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-// JavaScript Document
-Integer parallaxPage = 0;
-
-function setPage(Integer pagenum){
-    console.log(pagenum)
+function toggleDisplay(elementId) {
+	var x = document.getElementById(elementId);
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
 }
-
-function onPageLoad(){
-    inView('.intro').on('enter', console.log(1));
-    inView('.static1').on('enter', console.log(2));
-    inView('.bg2').on('enter', console.log(3));
-    inView('.static2').on('enter', console.log(4));
-}
-
-function pageScroll() {
-    window.scrollBy(0,1);
-    scrolldelay = setTimeout(pageScroll,10);
-}
-
-
-
